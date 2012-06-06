dbc1e0e44bf406829bf97c26ec6b7d97525bbb19
Prof1983 <prof1983@yandex.ru>
2012-06-06 09:59:49 +0600

    Added AiKernel_Init(); Added AiKernel.h file

---
+++ AiKernel.h
@@ -0,0 +1,16 @@
+/*  AiKernel header
+Author(Prof1983 prof1983@ya.ru)
+Created(06.06.2012)
+LastMod(06.06.2012)
+Version(0.0)
+*/
+
+#include "ABase2.h"
+
+/** Initializes agenda
+	@return(AError value) */
+func Agenda_Init();
+
+/** Initializes AiKernel
+	@retunr(AError value) */
+func AiKernel_Init();
--- AiKernelMain.c
+++ AiKernelMain.c
@@ -7,6 +7,25 @@ Version(0.0)
 
 #pragma argsused
 
+#define A_BUILD_DLL
+
+#include "AiKernel.h"
+
+func Agenda_Init()
+{
+	return 0;
+}
+
+// ----
+
+func AiKernel_Init()
+{
+	if (Agenda_Init() < 0) return -2;
+	return 0;
+}
+
+// ----
+
 int _libmain(unsigned long reason) {
 	return 1;
 }
