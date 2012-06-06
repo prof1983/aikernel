f64dec8be84f9cf7ad2297eeff131dacb92b9609
Prof1983 <prof1983@yandex.ru>
2012-06-06 09:25:25 +0600

    Added XE project files

---
+++ AiKernelMain.c
@@ -0,0 +1,12 @@
+/*  AiKernel main
+Author(Prof1983 prof1983@ya.ru)
+Created(06.06.2012)
+LastMod(06.06.2012)
+Version(0.0)
+*/
+
+#pragma argsused
+
+int _libmain(unsigned long reason) {
+	return 1;
+}
